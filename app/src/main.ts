c18d98cb0f68d43efb25abb91d285e8d5022d247
taijingming <1255274497@qq.com>
2022-04-03 10:22:34 +0800

    feat: 初始化框架

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Element from 'element-plus'
 
-createApp(App).use(store).use(router).mount('#app')
+const app: any = createApp(App)
+app.use(Element)
+app.use(store)
+app.use(router)
+app.use(router)
+app.mount('#app')
+// window.globalThis.vm = app
